8de46c5f331d4d85ed489acfaf7e0e1840f786e1
fischz <fischzac@gmail.com>
2021-03-03 14:45:16 -0500

    updated 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Layout from "../components/layout/Layout"
+
+const NotFoundPage: React.FC = () => {
+  return <Layout>
+    <h2>Page Not Found.</h2>
+  </Layout>
+}
+
+export default NotFoundPage
