db5de793b9f4d6260e37834f722b12ca6d24668e
fischz <fischzac@gmail.com>
2021-03-03 14:48:21 -0500

    updated page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import styled from 'styled-components';
+
 import Header from '../components/header/Header'
 import Intro from '../components/intro/Intro'
 import Layout from '../components/layout/Layout'
 import Projects from '../components/projects/Projects'
-import { ContentContainer, PageContainer } from './index.styles'
 
 
+export const PageContainer = styled.div`
+    display: flex;
+    @media screen and (max-width: 576px) {
+        flex-direction: column;
+        text-align: center;
+    }
+`
+
+export const ContentContainer = styled.div`
+    width: 100%;
+    flex: 1 0 0;
+`
 
 const IndexPage: React.FC = () => {
     return <Layout>
